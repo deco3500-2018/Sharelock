944194e0ccbc5ee56a12ad09dc083105351b30b5
wanningtian12 <42561157+wanningtian12@users.noreply.github.com>
2018-10-26 14:21:38 +1000

    upload index.php for live search

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>
+	<head>
+	    <meta name= "viewport" content="width=device-width, initial-scale=1"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Discover and Match Your Skill Mate</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="container">
+			<br />
+			<br />
+			<br />
+			<h2 align="center">Discover and Match Your Skill Mate</h2><br />
+			<div class="form-group">
+				<div class="input-group">
+					
+					<input type="text" name="search_text" id="search_text" placeholder="Search by the skills" class="form-control" />
+					<span class="input-group-addon">Search</span>
+				</div>
+			</div>
+			<br />
+			<div id="result"></div>
+		</div>
+		<div style="clear:both"></div>
+		<br />
+		
+		<br />
+		<br />
+		<br />
+	</body>
+</html>
+
+
+<script>
+$(document).ready(function(){
+	load_data();
+	function load_data(query)
+	{
+		$.ajax({
+			url:"fetch.php",
+			method:"post",
+			data:{query:query},
+			success:function(data)
+			{
+				$('#result').html(data);
+			}
+		});
+	}
+	
+	$('#search_text').keyup(function(){
+		var search = $(this).val();
+		if(search != '')
+		{
+			load_data(search);
+		}
+		else
+		{
+			load_data();			
+		}
+	});
+});
+</script>
+
+
+
+
